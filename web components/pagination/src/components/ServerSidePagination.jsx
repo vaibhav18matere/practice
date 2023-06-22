939be58e97404fd63d0bff357f573fc0da82a865
Vaibhav Matere <vpm00718897@gmail.com>
2023-06-22 12:52:31 +0530

    feat : added server side pagination

--- web components/pagination/src/components/ServerSidePagination.jsx
+++ web components/pagination/src/components/ServerSidePagination.jsx
@@ -1,7 +1,59 @@
-import React from "react";
-
+import { useState, useEffect } from "react";
+import Axios from 'axios';
 const ServerSidePagination = () => {
-  return <div>ServerSidePagination</div>;
+
+
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const apiUrl = `https://dummyjson.com/products?limit=10&skip=${page * 10 - 10}`;
+
+  const getProducts = async () => {
+    const res = await Axios.get(apiUrl);
+    if (res && res.data.products) {
+      setProducts(res.data.products);
+      setTotalPages(res.data.total / 10)
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, [page]);
+
+  const selectPageHandler = (selectedPage) => {
+    if (selectedPage >= 1 && selectedPage <= totalPages && selectedPage !== page)
+      setPage(selectedPage)
+  }
+
+  return <>
+    <a href="client"><button>Client Side Pagination</button></a>
+    <a href="server"><button>Server Side Pagination</button></a>
+    {products.length > 0 && (
+      <div className="products">
+        {products.map((prod) => {
+          return (
+            <div className="products__single" key={prod.id}>
+              <span>{prod.id}</span>
+              <div>{prod.title}</div>
+              <img src={prod.thumbnail} alt={prod.title} />
+            </div>
+          );
+        })}
+      </div>
+    )}
+    {
+      products.length > 0 && (
+        <div className='pagination'>
+          <span className={page <= 1 ? "pagination__disable" : ""} onClick={() => selectPageHandler(page - 1)} >previous</span>
+          {[...Array(totalPages)].map((_, index) => {
+            return <span className={page === index + 1 ? "pagination__selected" : ""} onClick={() => selectPageHandler(index + 1)} key={index} > {index + 1} </span>
+          })}
+          <span className={page < totalPages ? "" : "pagination__disable"} onClick={() => selectPageHandler(page + 1)} >next</span>
+        </div>
+      )
+    }
+  </>;
 };
 
 export default ServerSidePagination;
