baa1736b2a1d8a01258ef1149269270236335213
VaibhavMatere <vpm00718897@gmail.com>
2026-02-04 23:42:17 +0530

    string methods

---
+++ python/string_methods.py
@@ -0,0 +1,20 @@
+# String Methods
+
+first_name = "VaibhaV"
+last_name = "Matere"
+user_name = "   Whitespaces at Start"
+
+print(first_name.lower())
+print(first_name.upper())
+print(first_name.title())
+
+print(user_name.strip())
+
+print(user_name.find("at"))
+
+print(first_name.replace("Vai", "Bhav"))
+
+print("Vai" in user_name)
+print("Vai" in first_name)
+
+print("VAI" not in first_name)
