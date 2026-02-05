b55e60b90c2a15906c45c822dc8c83dbe3af6f63
VaibhavMatere <vpm00718897@gmail.com>
2026-02-05 21:50:36 +0530

    Conditional Statements

---
+++ python/conditional_statements.py
@@ -0,0 +1,14 @@
+# Conditional Statements
+
+temperature = 15
+# temperature = 25
+# temperature = 35
+
+if temperature >= 30:
+    print("It's too Hot")
+    print("Please take a water")
+elif temperature <= 20:
+    print("It's too Cold")
+else:
+    print("Temp is decent")
+print("Done!")
