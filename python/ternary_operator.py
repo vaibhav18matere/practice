46958e1973c667e26eae4fc176a375529298b0f3
VaibhavMatere <vpm00718897@gmail.com>
2026-02-05 21:51:01 +0530

    Ternary Operator

---
+++ python/ternary_operator.py
@@ -0,0 +1,15 @@
+# Ternary Operator
+
+# age = 22
+age = 32
+
+if age >= 25:
+    print("Eligible..!!")
+else:
+    print("Not Eligible...!!!")
+
+
+# Better way to write the same code
+
+message = "Can Vote" if age >= 18 else "Can not vote"
+print(message)
