fb0c5fc7718d310c70ed198bfeacc844700785a6
VaibhavMatere <vpm00718897@gmail.com>
2026-02-07 15:33:15 +0530

    Function

---
+++ python/functions.py
@@ -0,0 +1,18 @@
+# Function
+
+def greet():
+    print("Hello There!")
+
+
+greet()
+
+#  Difference between greet() function and print() function
+#  is that print() funciton takes input
+
+
+def user_greet(first_name, last_name):
+    print(f"Hello, {first_name} {last_name}")
+
+
+user_greet("Vaihbav", "Matere")
+user_greet("Sujata", "Thorat")
