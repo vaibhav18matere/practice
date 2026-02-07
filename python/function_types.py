75afcf7c4e33de7c1af19bd8f245de97ed6ee2be
VaibhavMatere <vpm00718897@gmail.com>
2026-02-07 19:49:10 +0530

    Function Types

---
+++ python/function_types.py
@@ -0,0 +1,22 @@
+# 1. Which performs tasks
+# 2. Which returns some values
+
+# 1 Which performs tasks
+
+
+def welcome_user():
+    print("Hello there!")
+
+
+welcome_user()
+
+# 2 Which returns some values
+
+
+def get_greeting(name):
+    return f"Hello {name}"
+
+
+message = get_greeting("VAIBHAV")
+
+print(message)
