a90d328c84fd582ad929e78276060ca97ed59cb5
VaibhavMatere <vpm00718897@gmail.com>
2026-02-08 23:16:58 +0530

    Mutable and Immutable

---
+++ python/mutable_and_immutable.py
@@ -0,0 +1,16 @@
+# Mutable:
+# List, Set, Dictionary, Byte Arrays
+
+# Immutable:
+# Integer, Float, Tuple, Frozenset, String, Bytes
+
+name = "Vaibhav"
+# print(name)
+
+name = "Sujata"
+print(name)
+
+a = 10
+b = a
+a = 15
+print(a, b)
