692ea9003509af6d9d6be16a5cc7dfe1fa754f6c
VaibhavMatere <vpm00718897@gmail.com>
2026-02-08 23:32:14 +0530

    Data types

---
+++ python/data_types.py
@@ -0,0 +1,44 @@
+# Data Types
+
+# Numbers
+# 1234
+# 3.123
+# a+bi
+# Decimal()
+
+# String
+# 'abcv'
+# "ab's"
+
+# List
+# my_list = [1, 2, 3, "four", [5, 6]]
+# list(range(10))
+
+# Tuples
+# (1, "spam", 4, "B")
+# tuple("anc")
+# namedtuple
+
+# Dictionary
+# abc = {"a": "b", "c": "d"}
+# dict(dours=10)
+
+# Set
+# set("abc"),
+# {'a', 'b', 'c'}
+
+# Boolean
+# True, False
+
+# None
+
+# Functions
+
+# Modules
+
+# Classes
+
+# Decorators
+# Generators
+# Iterators
+# Meta Programming
