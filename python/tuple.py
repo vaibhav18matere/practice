19e220e82315cb4aa49963f57187103099bd1dba
VaibhavMatere <vpm00718897@gmail.com>
2026-02-09 23:42:11 +0530

    Tuple

---
+++ python/tuple.py
@@ -0,0 +1,15 @@
+# Tuple
+
+company = ("Marketing", "Sales", "Accounting")
+
+print(company)
+
+print(company[-1])
+print(company[:])
+print(len(company))
+
+more_dept = ("SEO", "Content", "Coding")
+
+all_dept = company + more_dept
+
+print(all_dept)
