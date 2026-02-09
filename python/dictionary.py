a56fcceb91ec9c4bebfc71847d8ce71f16699dd1
VaibhavMatere <vpm00718897@gmail.com>
2026-02-09 22:43:08 +0530

    For loop in dictionary

--- python/dictionary.py
+++ python/dictionary.py
@@ -2,10 +2,16 @@
 
 # Order is not important , key is important
 
-user = {"name":"vaibhav", "age":23, "gender":"male"}
+user = {"name": "vaibhav", "age": 23, "gender": "male"}
 print(user)
 
 print(user.get("name"))
 
 new_age = user["age"] = 28
 print(new_age)
+
+for x in user:
+    print('key name is', x)
+
+for y in user:
+    print(y, user[y])
